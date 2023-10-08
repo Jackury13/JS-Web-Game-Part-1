52d1f5200be23d82ffa71a368df7f68532e95300
Jackury13 <143567012+Jackury13@users.noreply.github.com>
2023-10-08 14:17:31 -0700

    commit

--- index.js
+++ index.js
@@ -4,3 +4,44 @@ greenCharacter.style.position = 'fixed'
 greenCharacter.style.left = '100px'
 greenCharacter.style.bottom = '100px'
 document.body.append(greenCharacter)
+
+
+//pine tree
+let pineTree = document.createElement('img')
+pineTree.src = 'assets/pine-tree.png'
+pineTree.style.position = 'fixed'
+pineTree.style.left = '450px'
+pineTree.style.bottom = '200px'
+document.body.append(pineTree)
+
+//Tree
+let tree = document.createElement('img')
+tree.src = 'assests/tree.png'
+tree.style.position = 'fixed'
+tree.style.left = '200px'
+tree.style.bottom = '300px'
+tree.body.append(tree)
+
+//pillar
+let pillar = document.createElement('img')
+pillar.src = 'assets/pillar.png'
+pillar.style.position = 'fixed'
+pillar.style.left = '350px'
+pillar.style.bottom = '100px'
+document.body.append(pillar)
+
+//crate
+let crate = document.createElement('img')
+crate.src = 'assets/crate.png '
+crate.style.position = 'fixed'
+crate.style.left = '150px'
+crate.style.bottom = '200px'
+document.body.append(crate)
+
+//well
+let well = document.createElement('img')
+well.src = 'assets/well.png'
+well.style.position = 'fixed'
+well.style.left = '500px'
+well.style.bottom = '425px'
+document.body.append(well)
